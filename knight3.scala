2b56164eb5103e4d1f4abe5191f586c750a9fa27
Khalil Ahmed <mkhalila@outlook.com>
2016-11-22 00:35:02 +0000

    Implements ordered_moves

--- knight3.scala
+++ knight3.scala
@@ -12,7 +12,36 @@ type Path = List[Pos]    // a path...a list of positions
 // rule. That means moves with the fewest legal onward moves 
 // should come first.
 
-def ordered_moves(dim: Int, path: Path, x: Pos): List[Pos] = ..
+def is_legal(dim: Int, path: Path)(x: Pos): Boolean = {
+  val isInside = (x._1 >= 0) && (x._1 < dim) && (x._2 >= 0) && (x._2 < dim)
+  val isInPath = path.find(y => y == x).isDefined
+  isInside && !isInPath
+}
+
+def smaller_than(x1: Pos, x2: Pos, dim: Int, path: Path): Boolean =
+  legal_moves(dim, path, x1).size < legal_moves(dim, path, x2).size
+
+
+def legal_moves(dim: Int, path: Path, x: Pos): List[Pos] = {
+  val one = (x._1 + 1, x._2 + 2)
+  val two = (x._1 + 2, x._2 + 1)
+  val three = (x._1 + 2, x._2 - 1)
+  val four = (x._1 + 1, x._2 - 2)
+  val five = (x._1 - 1, x._2 - 2)
+  val six = (x._1 - 2, x._2 - 1)
+  val seven = (x._1 - 2, x._2 + 1)
+  val eight = (x._1 - 1, x._2 + 2)
+
+  List(one, two, three, four, five, six, seven, eight)
+    .filter(move => is_legal(dim, path)(move))
+}
+
+def ordered_moves(dim: Int, path: Path, x: Pos): List[Pos] = {
+  val legalMoves = legal_moves(dim, path, x)
+  legalMoves.sortWith(smaller_than(_, _, dim, path))
+}
+
+ordered_moves(6, List((0,0)), (2, 3))
 
 //(3b) Complete the function that searches for a single *closed* 
 // tour using the ordered moves function.
